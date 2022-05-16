2afa43d2a6ab69fc73190acaa3394ac3b4c63bc9
AngelosGR21 <angelgrigoriu@gmail.com>
2022-05-16 17:25:21 +0100

    adding catch block

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,10 @@
 const { fetchUsers } = require("../models/users");
 
 
-exports.getUsers = (req,res) => {
+exports.getUsers = (req, res, next) => {
     fetchUsers().then((users) => {
         res.status(200).send({users});
+    }).catch((err) => {
+        next(err)
     })
 }
