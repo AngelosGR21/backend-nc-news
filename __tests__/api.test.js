ae53a1fc608ca40d16d6244cf454059042408dda
AngelosGR21 <angelgrigoriu@gmail.com>
2022-05-16 14:46:06 +0100

    completed dynamic endpoint /api/articles/:id with tests and error handling

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -30,6 +30,38 @@ describe("GET /api/topics", () => {
     })
 })
 
+describe("GET /api/articles/:article_id && Testing error handling", () => {
+    test("Returns an object with the article found, the article should contain all 6 properties", () => {
+        return request(app).get("/api/articles/1").expect(200).then((res) => {
+            const { article } = res.body;
+
+            expect(article).toMatchObject({
+                author : expect.any(String),
+                title : expect.any(String),
+                article_id : expect.any(Number),
+                body : expect.any(String),
+                topic : expect.any(String),
+                created_at : expect.any(String),
+                votes : expect.any(Number)
+            })
+        })
+    })
+    test("Returns 404 when the article with the id specified was not found", () => {
+        return request(app).get("/api/articles/9999").expect(404).then((res) => {
+            const { message } = res.body;
+
+            expect(message).toBe("Article was not found")
+        })
+    })
+    test("Returns 400 when a wrong data type was inserted", () => {
+        return request(app).get("/api/articles/testingEndpoint").expect(400).then((res) => {
+            const { message } = res.body
+            
+            expect(message).toBe("Bad request");
+        })
+    })
+})
+
 
 describe("GET (inexistent endpoint) ~ Should return error", () => {
     test("/api/topicsss ~ 404", () => {
--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express")
 const app = express();
 
-const {getTopics} = require("./controllers/topics");
+const { getTopics } = require("./controllers/topics");
+const { getArticle } = require("./controllers/articles")
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticle)
 
 app.use("*", (req, res) => {
     res.status(404).send({message: "Endpoint not found"})
 })
+
+app.use((err, req, res, next) => {
+    if(err.code === "22P02"){
+        res.status(400).send({message: "Bad request"})
+    }else{
+        next(err);
+    }
+})
+
 
 app.use((err, req, res, next) => {
     if(err.status){
