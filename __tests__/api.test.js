a598fde62e75a9db26224f622fe555f882a225fc
AngelosGR21 <angelgrigoriu@gmail.com>
2022-05-17 14:35:30 +0100

    created endpoint /api/articles GET request with tests

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -143,6 +143,27 @@ describe("GET /api/users", () => {
     })
 })
 
+describe("GET /api/articles", () => {
+    test("Checking each each article structure", () => {
+        return request(app).get("/api/articles").expect(200).then((res) => {
+            const {articles} = res.body;
+            console.log(articles);
+            expect(articles).toHaveLength(12);
+            articles.forEach((article) => {
+                expect(article).toMatchObject({
+                    author: expect.any(String),
+                    title: expect.any(String),
+                    article_id : expect.any(Number),
+                    topic: expect.any(String),
+                    created_at : expect.any(String),
+                    votes: expect.any(Number),
+                    comment_count : expect.any(String),
+                })
+            })
+        })
+    })
+})
+
 describe("GET (inexistent endpoint) ~ Should return error", () => {
     test("/api/topicsss ~ 404", () => {
         return request(app).get("/api/topicsss").expect(404).then((res) => {
--- controllers/articles.js
+++ controllers/articles.js
@@ -1,4 +1,4 @@
-const { fetchArticle, updateArticle } = require("../models/articles");
+const { fetchArticle, updateArticle, fetchAllArticles } = require("../models/articles");
 
 exports.getArticle = (req, res, next) => {
     const { article_id } = req.params; 
@@ -15,6 +15,14 @@ exports.patchArticle = (req, res, next) => {
 
     updateArticle(article_id, inc_votes).then((updatedArticle) => {
         res.status(200).send({updatedArticle})
+    }).catch((err) => {
+        next(err);
+    })
+}
+
+exports.getAllArticles = (req, res, next) => {
+    fetchAllArticles().then((articles) => {
+        res.status(200).send({articles});
     }).catch((err) => {
         next(err);
     })
