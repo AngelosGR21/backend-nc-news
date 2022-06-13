85644ec4e9e58f1fcf19c20bc4f50a76d325050e
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-13 10:55:59 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const articleRouter = require("./routers/articleRouter");
 const topicsRouter = require("./routers/topicsRouter");
@@ -7,36 +8,33 @@ const commentsRouter = require("./routers/commentsRouter");
 const usersRouter = require("./routers/usersRouter");
 
 const {
-    psqlErrorHandler,
-    customErrorHandler,
-    serverErrorHandler
- } = require("./errors");
+  psqlErrorHandler,
+  customErrorHandler,
+  serverErrorHandler,
+} = require("./errors");
 
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/api", (req, res, next) => {
-    const data = require("./endpoints.json");
-    if(data){
-        res.status(200).send(data);
-    }else{
-        next()
-    }
-    
-})
+  const data = require("./endpoints.json");
+  if (data) {
+    res.status(200).send(data);
+  } else {
+    next();
+  }
+});
 app.use("/api/articles", articleRouter);
-app.use("/api/topics", topicsRouter)
-app.use("/api/users", usersRouter)
-app.use("/api/comments", commentsRouter)
+app.use("/api/topics", topicsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/comments", commentsRouter);
 
 app.use("*", (req, res) => {
-    res.status(404).send({message: "Endpoint not found"})
-})
+  res.status(404).send({ message: "Endpoint not found" });
+});
 
-app.use(psqlErrorHandler)
-app.use(customErrorHandler)
-app.use(serverErrorHandler)
-
-
+app.use(psqlErrorHandler);
+app.use(customErrorHandler);
+app.use(serverErrorHandler);
 
 module.exports = app;
