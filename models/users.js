949001bfbf2493c490a8595135a03383501248e4
AngelosGR21 <angelgrigoriu@gmail.com>
2022-07-10 21:18:10 +0100

    created fetchSingleUser

--- models/users.js
+++ models/users.js
@@ -5,3 +5,12 @@ exports.fetchUsers = () => {
         return rows;
     })
 }
+
+exports.fetchSingleUser = (username) => {
+    return db.query("SELECT * FROM users WHERE username = $1", [username])
+    .then((res) => {
+        if(res.rowCount) return res.rows[0];
+        
+        return Promise.reject({status: 404, message: "User was not found"});
+    })
+}
